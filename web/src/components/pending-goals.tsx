75c890ee6f146dfbf76a395b59de8e4a37b25661
Gui Silva <adrianon10@gmail.com>
2024-11-09 04:54:52 -0300

    feat: add help function handle handleCompletedGoal

---
+++ web/src/components/pending-goals.tsx
@@ -0,0 +1,40 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { Plus } from 'lucide-react'
+import { createGoalCompletion } from '../http/create-goal-completion'
+import { getPedingGoals } from '../http/get-pending-goals'
+import { OutlineButton } from './ui/outline-button'
+
+export const PendingGoals = () => {
+  const queryClient = useQueryClient()
+  const { data } = useQuery({
+    queryKey: ['peding-goals'],
+    queryFn: getPedingGoals,
+    staleTime: 1000 * 60, //60 seconds
+  })
+
+  if (!data) {
+    return null
+  }
+
+  const handleCompletedGoal = async (goalId: string) => {
+    await createGoalCompletion(goalId)
+    queryClient.invalidateQueries({ queryKey: ['summary', 'peding-goals'] })
+  }
+
+  return (
+    <div className="flex flex-wrap gap-3">
+      {data.map(goal => {
+        return (
+          <OutlineButton
+            key={goal.id}
+            disabled={goal.completionCount >= goal.desireWeeklyFrequency}
+            onClick={() => handleCompletedGoal(goal.id)}
+          >
+            <Plus className="size-4 text-zinc-600" />
+            {goal.title}
+          </OutlineButton>
+        )
+      })}
+    </div>
+  )
+}
